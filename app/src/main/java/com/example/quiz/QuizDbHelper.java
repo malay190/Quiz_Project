0ee169fe2c7886f64c162f4c30ce3b2e4b780d03
malay190 <malaykumar089@gmail.com>
2020-08-11 14:02:33 +0530

    Questions are shown on the screen, radio button working,show score and question number.

--- app/src/main/java/com/example/quiz/QuizDbHelper.java
+++ app/src/main/java/com/example/quiz/QuizDbHelper.java
@@ -2,11 +2,19 @@ package com.example.quiz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.quiz.QuizContract.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizDbHelper extends SQLiteOpenHelper {
+    public static final String TAG = "QuizDbHelper";
     private static final String DATABASE_NAME = "Quiz.db";
     private static final int DATABASE_VERSION = 1;
 
@@ -30,14 +38,18 @@ public class QuizDbHelper extends SQLiteOpenHelper {
                 ")";
 
         db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        Log.d(TAG, "onCreate");
         fillQuestionsTable();
 
     }
 
+    //SQLiteOpenHelper Called when the database needs to be upgraded. The implementation should use this method to drop tables, add tables,
+    // or do anything else it needs to upgrade to the new schema version.
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
         onCreate(db);
+        Log.d(TAG, "onUpgrade ");
 
     }
 
@@ -64,6 +76,27 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
         cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
         db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Questions> getAllQuestion() {
+        List<Questions> questionsList = new ArrayList<>();
+        db = getReadableDatabase();
+        Log.d(TAG, "getAllQuestion: db created ");
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Questions question = new Questions();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                questionsList.add(question);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return questionsList;
     }
 
 }
